0bd7946cfba644bc519027bef73a2280bb32bd75
Jiyun Cho <144382660+Jiyun2025@users.noreply.github.com>
2026-08-30 11:14:28 -0400

    Time: 606 ms (7.65%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,5 +1,6 @@
 /* Write your PL/SQL query statement below */
 SELECT x,y,z, 
+-- 가장 긴 한변의 길이가 나머지 두변의 합보다 작아야함 
 CASE WHEN x+y>z and y+z>x and x+z>y THEN 'Yes'
     ELSE 'No' END
      AS triangle 
