dc95deaea25381ec4e1ebeb31a9a30d1e41a11a4
Jiyun Cho <144382660+Jiyun2025@users.noreply.github.com>
2026-08-30 11:08:47 -0400

    Time: 608 ms (39.17%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -2,6 +2,7 @@
 SELECT id, 
     CASE 
     WHEN p_id IS NULL THEN 'Root'
+    -- is the child's ID in parent's node ID?  
     WHEN id IN (select t.p_id from Tree t) THEN 'Inner'
     ELSE 'Leaf' END as type
 FROM Tree 
